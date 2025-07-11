fb5ba0d6c55a147e8008f83cd5005ad143554388
Agustin altamirano <144826537+AgustinAltamirano10@users.noreply.github.com>
2025-07-10 22:07:36 -0300

    Update DoublyLinkedStack.java

--- practica5/tads/stack/src/main/java/DoublyLinkedStack.java
+++ practica5/tads/stack/src/main/java/DoublyLinkedStack.java
@@ -1,26 +1,23 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class DoublyLinkedStack<T> implements Stack<T> {
-
-
-    private class Node {
+public class StackDoublyLinkedList<T> implements Stack<T> {
+    
+    private class Node{
         Node next;
         Node prev;
         T data;
 
-        Node(T data) {
+        Node(T data){
             this.data = data;
         }
-        
     }
 
     private Node head;
     private Node tail;
     private int size;
 
-    public DoublyLinkedStack()
-    {
+    public StackDoublyLinkedList(){
         head = null;
         tail = null;
         size = 0;
@@ -33,41 +30,44 @@ public class DoublyLinkedStack<T> implements Stack<T> {
 
     @Override
     public boolean isEmpty() {
-        return head == null;
+        return size == 0;
     }
 
     @Override
     public void push(T e) {
         Node aux = new Node(e);
         aux.next = head;
-        aux.prev = null;
-        
-        if (isEmpty()) {
-            tail = aux;
-        } else {
-            head.prev = aux;
+
+        if (head != null) {
+            head.prev = aux; // conectamos el viejo head hacia atrás
         }
 
         head = aux;
+
+        if (size == 0) {
+            tail = aux; // en la primera inserción, head y tail son lo mismo
+        }
         size++;
     }
 
+
     @Override
     public T pop() {
-            if (isEmpty()) {
-                throw new NoSuchElementException();
-            }
-    
-            T data = head.data;
-            if (head == tail) {
-                head = null;
-                tail = null;
-            } else {
-                head = head.next;
-                head.prev = null;
-            }
-            return data;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        T element = head.data;
+        head = head.next;  // Movemos el head al siguiente nodo
+        if (head == null) { // Si la pila ahora está vacía
+            tail = null;
+        } else {
+            head.prev = null;  // Desconectamos el nodo anterior del nuevo head
+        }
+        size--;
+        return element;
     }
+
 
     @Override
     public T top() {
@@ -82,36 +82,33 @@ public class DoublyLinkedStack<T> implements Stack<T> {
         return new Iterator<T>() {
             private Node current = head;
 
-            public boolean hasNext()
-            {
+            public boolean hasNext() {
                 return current != null;
             }
 
-            public T next()
-            {
+            public T next() {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-
                 T data = current.data;
                 current = current.next;
-                return data; 
+                return data;
             }
         };
     }
-    
-    public String toString()
-    {
+
+
+    public String toString(){
         String res = "[";
         Node aux = head;
         while (aux != null) {
-            res+= aux.data;
+            res += aux.data;
             if (aux.next != null) {
-                res+= ", ";
-            } 
+                res += ", ";
+            }
             aux = aux.next;
         }
-        res+= "]";
+        res += "]";
         return res;
     }
 }
